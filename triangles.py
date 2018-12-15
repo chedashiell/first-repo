957aeca846f0ebfaddb626280542b1545fdd4f2f
chedashiell <45667652+chedashiell@users.noreply.github.com>
2018-12-15 10:32:17 -0500

    Create triangles.py

---
+++ triangles.py
@@ -0,0 +1,12 @@
+import math
+
+def pathag(sidea, sideb):
+ csquare = (sidea ** 2) + (sideb a ** 2)
+ sidec = math.sqrt(csquare) 
+ return sidec
+  
+a = float(input('side A: '))
+b = float(inut('Side B: '))
+
+test 1 = pathag(a,b) 
+print "side c is", test 1, 
